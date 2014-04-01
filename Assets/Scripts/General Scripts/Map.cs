970024c29fa74f10d27ed53b368bc7f14b03af65
jillsthebest <jill.c.russek@gmail.com>
2014-04-01 01:29:43 -0400

    Player
    -needs fixing, but instantiates player on may
    -probably create center

--- Assets/Scripts/General Scripts/Map.cs
+++ Assets/Scripts/General Scripts/Map.cs
@@ -11,12 +11,15 @@ public class Map : MonoBehaviour {
 
 	void Start(){
 		this.createMap();
+
 	}
 
 	public void Update(){
 		if(this.tileList.Count> 0){
 			this.selectTile();
 		}
+
+
 	}
 
 	public void createHexTile(float i, float j, int upDown, float widthAway){
@@ -44,7 +47,7 @@ public class Map : MonoBehaviour {
 			pos.y = loc.y;
 			break;
 		}
-
+		hextile.position = pos;
 		newT.transform.position = pos;
 		hextile.location = loc;
 		hextile.setWidthAndHeight();
--- Assets/Scripts/General Scripts/WorldManager.cs
+++ Assets/Scripts/General Scripts/WorldManager.cs
@@ -4,12 +4,15 @@ using System.Collections;
 public class WorldManager : MonoBehaviour {
 	
 	public Map map;
+	public Player player;
+	public Sprite playerSprite;
 
 	// Use this for initialization
 	void Start () {
-		//this.createMap();
-		//this.map = new Map();
+		this.map = GameObject.FindGameObjectWithTag("Map").GetComponent<Map>();
+		this.player = new Player(map.tileList, playerSprite);
 
+		//player.SendMessage("crecreatePlayerInRandomLocation", map.tileList);
 	}
 	
 	// Update is called once per frame
