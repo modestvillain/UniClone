b109959bcde5ac12ef5401df071088f73ce7670e
jzzur2000 <jzur2000@gmail.com>
2014-04-04 18:16:34 -0400

    temporary AI framework

--- Assets/Scripts/General Scripts/AI.cs
+++ Assets/Scripts/General Scripts/AI.cs
@@ -5,11 +5,55 @@ public class AI : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	public tuple[] getMoveList(Team myTeam,HexTile[] tileList, int credits){
+		//get Legal Moves
+			//get all friendly troops
+		Player[] listOfFriendlies = myTeam.getTroops ();
+
+			//get list of legal destinations
+		for (int i=0; i<listOfFriendlies.Length; i++) {
+
+
+			}
+			//get list of legal attack targets
+
+		//select moves randomly
+		float rand = Random.Range (0.0f, 1.0f);
+
+		actionList = new tuple[3];
+		return actionList;
+
+	}
+
+public class tuple{
+		private Player attacker;
+		private Player target;
+		private HexTile destination;
+
+		void start(Player attckr, HexTile dest, Player targ){
+			attacker = attckr;
+			destination = dest;
+			target = targ;
+		}
+
+		public Player getAttacker(){
+			return attacker;
+		}
+
+		public Player getTarget(){
+			return target;
+		}
+
+		public HexTile getDestination(){
+			return destination;
+		}
 	}
 }
