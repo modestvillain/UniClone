ec34026683affeb2681a9d1bbbc4223cb7bfc407
modestvillain <tyler.d.stowell@gmail.com>
2014-04-04 16:30:39 -0400

    Random additions

--- Assets/Scripts/General Scripts/Player.cs
+++ Assets/Scripts/General Scripts/Player.cs
@@ -5,27 +5,22 @@ using System.Collections.Generic;
 public class Player:MonoBehaviour  {
 	public GameObject player;
 	public Sprite normalSprite;
-	int numMoveTiles = 1;
-	//GameObject Map;
-
-
-
-
-
-
-
-
+	public int numMoveTiles = 1;
+	public int HP;
+	public int DMG;
+	public int DEF;
+	public int MOB;
+	
 	public void move(GameObject hextile){
 
 		//set the location equal to the corresponding tile's position
-		//if checking the map reveals that the move is okay{
+		//if checking the map reveals that the move is okay
 		HexTile hexscript = hextile.GetComponent<HexTile>();
 		this.player.transform.position = hexscript.center;
 		//remove self from occupant of previous tile
 		//put this one as occupied
 		hexscript.occupant = this.player;
 		hexscript.deselect();
-		//}
 
 	}//move
 }
