6e61d065a61c6665f1148a72d120e826d6c5bf88
modestvillain <tyler.d.stowell@gmail.com>
2014-04-22 18:40:30 -0400

    Merge branch 'master' of https://github.com/modestvillain/UniClone
    Conflicts:
    	Assets/Resources/Prefabs/waterTile.prefab
    	Assets/Scripts/General Scripts/DummyAI.cs
    	Assets/Scripts/General Scripts/Player.cs
    	Assets/Scripts/General Scripts/WorldManager.cs

--- Assets/Scripts/General Scripts/DummyAI.cs
+++ Assets/Scripts/General Scripts/DummyAI.cs
@@ -14,8 +14,8 @@ public class DummyAI :MonoBehaviour {
 	private bool created = false;
 
 	void OnEnable() {
-		TM = GameObject.FindGameObjectWithTag("RED").GetComponent<TeamManager>();
-		BLUE = GameObject.FindGameObjectWithTag("BLUE").GetComponent<TeamManager>();
+		TM = WorldManager.redScript;
+		BLUE = WorldManager.blueScript;
 		types[0] = AERIAL;
 		types[1] = SOLDIER;
 		types[2] = HEAVY;
@@ -30,73 +30,66 @@ public class DummyAI :MonoBehaviour {
 				createNewPlayer(TM.bases[0],types[UnityEngine.Random.Range(0,3)]);
 			}
 			if(TM.team.Count > 0 & !created) {
-				for(int i=0; i<TM.team.Count; i++) {
-					List<HexTile> possibleMoves = WorldManager.map.legalMoves(TM.team[i]);
-					HexTile opTile = null;
-					int minDistance = 100;
-					foreach(HexTile ht in possibleMoves) {
-						int newMin = (int)Math.Sqrt((int)Math.Pow(ht.x-BLUE.bases[0].x,2) + (int)Math.Pow(ht.y-BLUE.bases[0].y,2));
-						if(newMin < minDistance) {
-							opTile = ht;
-							minDistance = newMin;
-						}
-					}
-					if(opTile != null) {
-						if(opTile.gameObject.tag == "Base") {
-							TM.team[i].capture((Base)opTile);
-							if(BLUE.bases.Count==0) {
-								break;
-							}
-						}
-						else {
-							TM.team[i].move(opTile.gameObject);
-						}
-					}
-					List<HexTile> possibleAtts = WorldManager.map.legalAttacks(TM.team[i]);
-					if(possibleAtts.Count>0) {
-						GameObject p = possibleAtts[UnityEngine.Random.Range(0,possibleAtts.Count)].occupant;
-						TM.team[i].attack((Player)p.GetComponent(p.tag));
-					}
-				}
+				doActions (0);
 			}
 			else if(TM.team.Count > 0) {
-				for(int i=0; i<TM.team.Count-1; i++) {
-					List<HexTile> possibleMoves = WorldManager.map.legalMoves(TM.team[i]);
-					HexTile opTile = null;
-					int minDistance = 100;
-					foreach(HexTile ht in possibleMoves) {
-						int newMin = (int)Math.Sqrt((int)Math.Pow(ht.x-BLUE.bases[0].x,2) + (int)Math.Pow(ht.y-BLUE.bases[0].y,2));
-						if(newMin < minDistance) {
-							opTile = ht;
-							minDistance = newMin;
-						}
-					}
-					if(opTile != null) {
-						if(opTile.gameObject.tag == "Base") {
-							TM.team[i].capture((Base)opTile);
-							if(BLUE.bases.Count==0) {
-								break;
-							}
-						}
-						else {
-							TM.team[i].move(opTile.gameObject);
-						}
-					}
-					List<HexTile> possibleAtts = WorldManager.map.legalAttacks(TM.team[i]);
-					if(possibleAtts.Count>0) {
-						GameObject p = possibleAtts[UnityEngine.Random.Range(0,possibleAtts.Count)].occupant;
-						TM.team[i].attack((Player)p.GetComponent(p.tag));
-					}
-				}
+				doActions (1);
 			}
 		}
 		endAITurn();
 	}
 
+	public void doActions(int off) {
+
+		State s = new State(WorldManager.map.tileList,WorldManager.blueScript,WorldManager.redScript);
+
+		for(int i=0; i<s.red.team.Count-off; i++) {
+
+			List<HexTile> possibleMoves = WorldManager.map.legalMoves(s.red.team[i]);
+			HexTile opTile = null;
+			double bestScore = -9999999;
+
+			foreach(HexTile ht in possibleMoves) {
+//				int newMin = (int)Math.Sqrt((int)Math.Pow(ht.x-BLUE.bases[0].x,2) + (int)Math.Pow(ht.y-BLUE.bases[0].y,2));
+//				if(newMin < minDistance) {
+//					opTile = ht;
+//					minDistance = newMin;
+//				}
+				State newS = new State(s);
+				newS.red.team[i].move(ht.gameObject);
+				double score = newS.scoringFunction();
+
+				if(score>bestScore) {
+					bestScore = score;
+					opTile = ht;
+				}
+			}
+
+
+			if(opTile != null) {
+				if(opTile.gameObject.tag == "Base") {
+					TM.team[i].capture((Base)opTile);
+					if(BLUE.bases.Count==0) {
+						break;
+					}
+
+				else {
+					TM.team[i].move(opTile.gameObject);
+				}
+			}
+			List<HexTile> possibleAtts = WorldManager.map.legalAttacks(TM.team[i]);
+			if(possibleAtts.Count>0) {
+				GameObject p = possibleAtts[UnityEngine.Random.Range(0,possibleAtts.Count)].occupant;
+				TM.team[i].attack((Player)p.GetComponent(p.tag));
+			}
+		}
+	}
+
 	private void endAITurn() {
-		GameObject.FindGameObjectWithTag("BLUE").GetComponent<TeamManager>().removePlayersFromCapturedBases();
-		GameObject.FindGameObjectWithTag("RED").GetComponent<TeamManager>().removePlayersFromCapturedBases();
-		WorldManager.beginPlayerTurn(me);
+
+		WorldManager.blueScript.removePlayersFromCapturedBases();
+		WorldManager.redScript.removePlayersFromCapturedBases();
+		WorldManager.beginPlayerTurn();
 	}
 
 	//creates player on top of base
@@ -106,4 +99,51 @@ public class DummyAI :MonoBehaviour {
 			created = true;
 		}
 	}
+
+	private class State {
+		
+		public List<HexTile> map;
+		public TeamManager blue;
+		public TeamManager red;
+		
+		public State(List<HexTile> oldmap, TeamManager oldblue, TeamManager oldred) {
+			map = new List<HexTile>(oldmap);
+			blue = copyTM(oldblue);
+			red = copyTM(oldred);
+		}
+		
+		public State(State s) {
+			map = new List<HexTile>(s.map);
+			blue = copyTM(s.blue);
+			red = copyTM(s.red);
+		}
+		
+		public TeamManager copyTM(TeamManager tm) {
+			TeamManager TM = new TeamManager(tm.parent);
+			TM.team = new List<Player>(tm.team);
+			TM.CREDITS = tm.CREDITS;
+			TM.bases = new List<Base>(tm.bases);
+			return TM;
+		}
+		
+		public double scoringFunction(){
+			double score = 0;
+			//total health of all players should be greater than the total health of theirs-- your total health minus theirs
+			score += red.totalHealth() - blue.totalHealth();
+			//money is good in the begining-- want enough money to buy something substantial, for now just have money
+			score += red.CREDITS;
+			// want a player that has high enough damage to take out the player's on their map (so want stronger players)-- your total damage minus theirs
+			score += red.totalDamage() - blue.totalDamage();
+			// better if total strength is better than theirs-- your total strength minus theirs, offense
+			score += red.totalDefense() - blue.totalDefense();
+			// better if you have more bases than they do -- your bases minus their bases
+			score += red.bases.Count - blue.bases.Count;
+			
+			//other things
+			//the proximity of individual troops
+			return score;
+		}//method
+		
+		
+	}//state class
 }
